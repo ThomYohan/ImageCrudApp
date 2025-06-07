e0ae54a53628355b1d59abc122560366a84f9ef7
Thomas Yohannes <thomasgyohannes@gmail.com>
2025-06-07 12:31:15 -0500

    ImageGrid initial file structure

---
+++ src/components/ImageGrid.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { getImages, deleteImage, ImageData } from '../api/mockApi';
+import ImageCard from './ImageCard';
+
+interface Props {
+    searchTerm: string;
+}
+
+const ImageGrid: React.FC<Props> = ({ searchTerm }) => {
+    const [images, setImages] = useState<ImageData[]>([]);
+
+    useEffect(() => {
+        setImages(getImages());
+    }, []);
+
+    const handleDelete = (id: string) => {
+        deleteImage(id);
+        setImages(getImages());
+    };
+
+    const filtered = images.filter((img) =>
+    img.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div>
+            {filtered.map((img) => (
+                <ImageCard key={img.id} image={img} onDelete={() => handleDelete(img.id)} />
+            ))}
+        </div>
+    );
+};
+
+export default ImageGrid
