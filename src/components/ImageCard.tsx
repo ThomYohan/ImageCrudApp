6d5ea7e3c13299d82e9021f5c77b72f7c0070233
Thomas Yohannes <thomasgyohannes@gmail.com>
2025-06-07 12:36:56 -0500

    ImageCard initial file structure

---
+++ src/components/ImageCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ImageData } from '../api/mockApi';
+
+interface Props {
+    image: ImageData;
+    onDelete: () => void;
+}
+
+const ImageCard: React.FC<Props> = ({ image, onDelete }) => {
+    return (
+        <div style={{ border: '1px solid #ccc', padding: '1rem', textAlign: 'center'}}>
+            <img
+            src={image.url}
+            alt={image.name}
+            style={{ width: '100%', height: '150px', objectFit: 'cover'}}
+            />
+            <p>{image.name}</p>
+            <button onClick={onDelete}>Delete</button>
+        </div>
+    );
+};
+
+export default ImageCard;
