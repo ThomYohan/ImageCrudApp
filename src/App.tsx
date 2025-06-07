400d2780db1234bebd985a9c9d93c9b4d9b5ef0e
Thomas Yohannes <thomasgyohannes@gmail.com>
2025-06-07 12:07:08 -0500

    Updated app setup with new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import SearchBar from './components/SearchBar'
+import ImageUploader from './components/ImageUploader'
+import ImageGrid from './components/ImageGrid'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState('')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif'}}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '1rem'}}>
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <ImageUploader />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h2> 4 Images</h2>
+      <ImageGrid searchTerm={searchTerm} />
+    </div>
   )
 }
 
